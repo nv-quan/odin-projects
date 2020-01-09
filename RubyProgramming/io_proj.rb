fc3b59cd24f629c7e76ee54820b937f76ef34e40
nv-quan <quannguyenvan1701@gmail.com>
2020-01-09 11:36:03 +0700

    Edit IO project

--- RubyProgramming/io_proj.rb
+++ RubyProgramming/io_proj.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require 'set'
 class CharacterGuessed < StandardError
   def initialize(msg = "Character already guessed", exception_type="custom")
@@ -60,10 +61,44 @@ class Hangman
     end
   end
 end
-words = File.readlines("5desk.txt")
+def get_choice(*args, **keyword_args) 
+  msg = args[0]
+  proc1 = args[1]
+  proc2 = args[2]
+  loop do
+    puts msg
+    choice = gets.chomp.match(/^[yYnN]$/)
+    if choice[0].downcase == 'y'
+      return proc1.call(keyword_args)
+    elsif choice[0].downcase == 'n'
+      return proc2.call(keyword_args)
+    else
+      puts "Invalid choice"
+    end
+  end
+end
 INCORRECT_GUESSES = 5
-hangman = Hangman.new(words)
-loop do
+load_game = lambda do |keyword_args|
+  filename = keyword_args[:filename]
+  File.open(filename, "r") do |save_file|
+    return YAML::load(save_file.read)
+  end
+end
+new_game = lambda do |keyword_args| 
+  words = File.readlines("5desk.txt")
+  return Hangman.new(words)
+end
+save_game = lambda do |keyword_args|
+  filename = keyword_args[:filename]
+  object = keyword_args[:object]
+  File.open(filename, 'w') do |save_file|
+    str = YAML::dump(object)
+    save_file.puts str
+  end
+  return 'close program'
+end
+game_play = lambda do |keyword_args|
+  hangman = keyword_args[:object]
   puts hangman
   puts "Guessed: #{hangman.guessed.to_a.to_s}"
   puts "#{INCORRECT_GUESSES - hangman.num_incorrect} incorrect time(s) left"
@@ -77,13 +112,18 @@ loop do
     end
     if hangman.finished? && hangman.num_incorrect <= INCORRECT_GUESSES
       puts "#{hangman.word.upcase}. You win."
-      break
+      return 'close program'
     elsif hangman.num_incorrect > INCORRECT_GUESSES
       puts "You lose."
       puts "The word is: #{hangman.word.upcase}"
-      break
+      return 'close program'
     end
   rescue StandardError => e
     puts e.message
   end
+end
+filename = 'hangmansave.yaml'
+hangman = get_choice("Do you want to load saved game? (y/n)", load_game, new_game, filename: filename)   
+loop do 
+  break if get_choice("Do you want to save the game? (y/n)", save_game, game_play, filename: filename, object: hangman) == 'close program'
 end
