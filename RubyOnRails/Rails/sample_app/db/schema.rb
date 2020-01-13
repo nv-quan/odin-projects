ec84595ae20564a20dfb7e6a60aa8afe514495c3
nv-quan <quannguyenvan1701@gmail.com>
2020-01-13 14:35:22 +0700

    Fix database stuff

--- RubyOnRails/Rails/sample_app/db/schema.rb
+++ RubyOnRails/Rails/sample_app/db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200113063016) do
+ActiveRecord::Schema.define(version: 20200113073050) do
+
+  create_table "structures", force: :cascade do |t|
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
