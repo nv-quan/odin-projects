09abf1ed70950f80437cc18ed75e396482387d4b
nv-quan <quannguyenvan1701@gmail.com>
2020-01-16 10:07:09 +0700

    Finish user signup

--- RubyOnRails/Rails/sample_app/db/schema.rb
+++ RubyOnRails/Rails/sample_app/db/schema.rb
@@ -9,6 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20200113063016) do
 
   create_table "users", force: :cascade do |t|
